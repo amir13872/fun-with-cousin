f95c97f9e143dee6eda1eff60ad4c7abc203fe4c
Amir Mahdi Zare <83335541+amir13872@users.noreply.github.com>
2025-01-31 12:41:15 +0330

    Update girlfriend.py

--- girlfriend.py
+++ girlfriend.py
@@ -21,13 +21,13 @@ def yes(): # teh function of yes
     message_label.config(text="I love you my pretty girlfriend, call me now")
 
  
-message_label = tk.Label(root, text="ایا خری",font = ("vazir bold", 14))
+message_label = tk.Label(root, text="Will you marry me?",font = ("vazir bold", 14))
 message_label.pack(pady=20)
 
-yes_botton = tk.Button(root,text="اره ",command=yes, font=("vazir bold", 12))
+yes_botton = tk.Button(root,text="YES",command=yes, font=("vazir bold", 12))
 yes_botton.pack(side="left", padx=12)
 
-no_botton = tk.Button(root,text="no ",command=no, font=("vazir bold", 12))
+no_botton = tk.Button(root,text="NO",command=no, font=("vazir bold", 12))
 no_botton.pack(side="right", padx=12)
 
 root.mainloop()
